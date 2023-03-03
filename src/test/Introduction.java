068b226cf67bc42ed90ba73cfdd090367a83a648
strdos <81235866+strdos@users.noreply.github.com>
2023-03-03 23:13:32 +0100

    TestNG - initial commit

---
+++ src/test/Introduction.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Introduction {
+
+    @Test
+    public void Demo() {
+        System.out.println("abc");
+    }
+
+    @Test
+    public void TestCase2() {
+        System.out.println("def");
+    }
+
+}
---
+++ src/test/Module2.java
@@ -0,0 +1,10 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Module2 {
+    @Test
+    public void test1() {
+        System.out.println("module 2 - test 1");
+    }
+}
