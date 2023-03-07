96ac2a199028070f6d4470f795ef1a0bac3a3d5b
strdos <81235866+strdos@users.noreply.github.com>
2023-03-07 20:38:44 +0100

    TestNG - parametrization, via .xml file and via @DataProvider annotation

--- src/test/Module2.java
+++ src/test/Module2.java
@@ -1,14 +1,13 @@
 package test;
 
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class Module2 {
+    @Parameters({"url"})
     @Test (groups = "Smoke")
-    public void test1() {
+    public void test1(String url) {
         System.out.println("module 2 - test 1");
+        System.out.println(url);
     }
     @BeforeTest
     public void runPrerequisiteAction() { System.out.println("prerequisite action");}
--- src/test/Module3.java
+++ src/test/Module3.java
@@ -1,12 +1,16 @@
 package test;
 
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class Module3 {
+    @Parameters({"url", "apikey"})
     @Test
-    public void calculateCarLoanWeb() {
+    public void calculateCarLoanWeb(String url, String apikey) {
         System.out.println("module 3 - test web");
+        System.out.println(url);
+        System.out.println(apikey);
     }
     @Test(enabled = false)
     public void calculateCarLoanMobile() {
@@ -19,5 +23,26 @@ public class Module3 {
     @Test(timeOut = 1000)
     public void calculateCarLoanExport() {
         System.out.println("module 3 - test export");
+    }
+    @Test(dataProvider="getData")
+    public void calculateCarLoanImport(String username, String password) {
+        System.out.println(username + " " + password);
+    }
+    @DataProvider
+    public Object[][] getData() {
+        // set 1 - username and psw as arguments
+        // set 2 - username and psw as arguments
+        // set n - username and psw as arguments
+        Object[][] data = new Object[3][2]; //3 is number of sets = number of times the test will run; 2 is number of args
+        // set 1
+        data[0][0] = "username1";
+        data[0][1] = "password1";
+        //set 2
+        data[1][0] = "username2";
+        data[1][1] = "password2";
+        //set 3
+        data[2][0] = "username3";
+        data[2][1] = "password3";
+        return data;
     }
 }
